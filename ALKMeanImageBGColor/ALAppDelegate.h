416b6503c59d784d7b310e5241b34a6d121e7e79
Alex Lee <liyiqiang@maxthon.net>
2014-09-10 16:43:45 +0800

    Initial Commit

---
+++ ALKMeanImageBGColor/ALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALAppDelegate.h
+//  ALKMeanImageBGColor
+//
+//  Created by Alex Lee on 9/10/14.
+//  Copyright (c) 2014 Alex Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ALKMeanImageBGColor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ALKMeanImageBGColor
+//
+//  Created by Alex Lee on 9/10/14.
+//  Copyright (c) 2014 Alex Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ALAppDelegate class]));
+    }
+}
